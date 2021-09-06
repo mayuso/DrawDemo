15227b459932e73abc9cefa8a74687cf7fcfc199
Mayuso <mikelayuso@gmail.com>
2021-09-06 22:09:14 +0200

    Added sources

---
+++ Assets/DrawController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class DrawController : MonoBehaviour
+{
+    public Camera m_camera;
+    public GameObject brushPrefab, pauseScreen;
+    LineRenderer currentLineRenderer;
+
+    Vector2 lastPos;
+
+    private void Update()
+    {
+        if(!pauseScreen.activeSelf)
+            Draw();
+    }
+
+    void Draw() 
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            CreateBrush();
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePos != lastPos) 
+            {        
+                AddAPoint(mousePos);
+                lastPos = mousePos;
+            }
+        }
+        else 
+        {
+            currentLineRenderer = null;
+        }
+    }
+
+    void CreateBrush() 
+    {
+        GameObject brushInstance = Instantiate(brushPrefab);
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+
+        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+
+        currentLineRenderer.SetPosition(0, mousePos);
+        currentLineRenderer.SetPosition(1, mousePos);
+
+    }
+
+    void AddAPoint(Vector2 pointPos) 
+    {
+        currentLineRenderer.positionCount++;
+        int positionIndex = currentLineRenderer.positionCount - 1;
+        currentLineRenderer.SetPosition(positionIndex, pointPos);
+    }
+}
---
+++ Assets/PauseScreen/PauseScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PauseScreenManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+    public void Exit()
+    {
+        pauseScreen.SetActive(false);
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            pauseScreen.SetActive(!pauseScreen.activeSelf);
+        }
+    }
+}
